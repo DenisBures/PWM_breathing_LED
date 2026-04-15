ea537648a49f1468efca75bf4fe2b08cec7c5c62
DenisBures <270772@vutbr.cz>
2026-04-15 19:07:43 +0200

    Update pwm_driver_1.vhd

--- pwm_driver_1.vhd
+++ pwm_driver_1.vhd
@@ -14,8 +14,19 @@ entity pwm_driver_1 is
 end pwm_driver_1;
 
 architecture Behavioral of pwm_driver_1 is
+--Konstanty    
+    constant C_PWM_WIDTH     : positive := 8;  
+    constant C_BREATH_MAX    : integer  := 1_000_000;
 
-signal sig_cnt : integer range 0 to G_MAX - 1;
+    signal sig_cnt  : integer range 0 to C_BREATH_MAX - 1;
+
+--Signály
+    signal s_ce_pwm    : std_logic;
+    signal s_pwm_cnt   : std_logic_vector(C_PWM_WIDTH - 1 downto 0);
+    signal s_duty_cycle: unsigned(C_PWM_WIDTH - 1 downto 0) := (others => '0');
+    signal s_direction : std_logic := '0'; -- 0 = nahoru, 1 = dolů
+
+signal sig_cnt : integer range 0 to C_BREATH_MAX - 1;
 
 component clk_en is
     generic (G_MAX : positive :=5); 
@@ -27,33 +38,63 @@ component clk_en is
 end component clk_en;
 
 component counter is
-    generic ( G_BITS : positive := 3 );  --! Default number of bits
+    generic ( G_BITS : positive := 3 );  
     port (
-        clk : in  std_logic;                             --! Main clock
-        rst : in  std_logic;                             --! High-active synchronous reset
-        en  : in  std_logic;                             --! Clock enable input
-        cnt : out std_logic_vector(G_BITS - 1 downto 0)  --! Counter value
+        clk : in  std_logic;                             
+        rst : in  std_logic;                             
+        en  : in  std_logic;                             
+        cnt : out std_logic_vector(G_BITS - 1 downto 0)  
     );
-end component counter;
-
+end component counter;   
+    
 begin
-    synchr_process : process(clk)
+    CE_PWM : clk_en 
+        generic map(G_MAX => 4) 
+        port map(clk => clk, 
+                 rst => rst, 
+                 ce => s_ce_pwm);
         
+    PWM_CNT_INST : counter 
+        generic map(G_BITS => C_PWM_WIDTH)
+        port map(clk => clk, 
+                 rst => rst, 
+                 en => s_ce_pwm, 
+                 cnt => s_pwm_cnt); 
+    
+synchr_process : process(clk)  
 begin
   if rising_edge(clk) then
-    if rst = '1' then
-      sig_cnt <= 0;
-        
-    else
-      if sig_cnt < sig_cnt_len - 1 then
-        sig_cnt <= sig_cnt + 1;
-      else
-        sig_cnt <= 0;
-      end if;
-        
-    end if;
-  end if;
-end process;
+            if rst = '1' then
+                sig_cnt <= 0;
+                s_duty_cycle <= (others => '0');
+                s_direction <= '0';
+            else
+                -- Čítač rychlosti dýchání
+                if sig_cnt < C_BREATH_MAX - 1 then
+                    sig_cnt <= sig_cnt + 1;
+                else
+                    sig_cnt <= 0;
+                    
+                    --nádech/výdech
+                    if s_direction = '0' then 
+                        if s_duty_cycle = 254 then 
+                            s_direction <= '1'; 
+                        end if;
+                        s_duty_cycle <= s_duty_cycle + 1;
+                    else 
+                        if s_duty_cycle = 1 then 
+                            s_direction <= '0'; 
+                        end if;
+                        s_duty_cycle <= s_duty_cycle - 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    
+    -- Porovnáváme běžící čítač s naší aktuální "dýchající" střídou
+    led <= '1' when (unsigned(s_pwm_cnt) < s_duty_cycle) else '0';
 
 
 end Behavioral;
